2dea8cf00e7edeb2fa699df12a31c8b5eee74246
faresG <guizanifares0@gmail.com>
2025-06-25 16:48:35 +0200

    efzf

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
+    unoptimized: true,
   },
   /* config options here */
   webpack(config) {
